859f7850ea09a27f9846dbec9e340a1ab7b31859
lky8967 <lky8967@gmail.com>
2022-06-07 18:42:26 +0900

    유니크 삭제

--- src/main/java/com/sparta/spring99_homework/model/Food.java
+++ src/main/java/com/sparta/spring99_homework/model/Food.java
@@ -24,7 +24,7 @@ public class Food {
     private Restaurant restaurant;
 
 
-    @Column(nullable = false , unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
